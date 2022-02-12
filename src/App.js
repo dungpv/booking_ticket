b4fd0955e83ba1e92f597849e654ddfb616e3496
dungpv <vietdungst@gmail.com>
2022-02-12 11:14:37 +0700

    TailwindCss: flex, padding. margin, width, height

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import DemoGrid from "./TailwindComponent/DemoGrid";
+import FlexDemo from "./TailwindComponent/FlexDemo";
+import PaddingMarginDemo from "./TailwindComponent/PaddingMarginDemo";
+import WidthHeightDemo from "./TailwindComponent/WidthHeightDemo";
 
 function App() {
   return (
     <div className="App">
-      <DemoGrid></DemoGrid>
+      {/* <DemoGrid></DemoGrid>
+      <PaddingMarginDemo></PaddingMarginDemo>
+      <WidthHeightDemo></WidthHeightDemo> */}
+      <FlexDemo></FlexDemo>
     </div>
   );
 }
