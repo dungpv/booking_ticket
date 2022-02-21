c281547f073f49d1bd5e9c02a6a2206446470fa5
dungpv <vietdungst@gmail.com>
2022-02-21 21:59:16 +0700

    BookingTicket: cau hinh redux

---
+++ src/redux/configStore.js
@@ -0,0 +1,6 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducers = combineReducers({});
+
+export const store = createStore(rootReducers, applyMiddleware(thunk));
