45aba8f08ed7d922e233723a5826db4aa1e777e7
dungpv <vietdungst@gmail.com>
2022-02-21 21:50:12 +0700

    remove file

--- src/App.js
+++ src/App.js
@@ -1,48 +1,7 @@
 import "./App.css";
-import BaiTapTongHop from "./BaiTapTongHop/BaiTapTongHop";
-import ChunkLodash from "./Lodash/ChunkLodash";
-import CompareObjectArray from "./Lodash/CompareObjectArray";
-import FillLodash from "./Lodash/FillLodash";
-import FlattenLodash from "./Lodash/FlattenLodash";
-import IncludeLodash from "./Lodash/IncludeLodash";
-import JoinDemo from "./Lodash/JoinDemo";
-import LastFirstLodash from "./Lodash/LastFirstLodash";
-import SortLodash from "./Lodash/SortLodash";
-import UniqLodash from "./Lodash/UniqLodash";
-import BaiTapLayoutTailwindCss from "./TailwindComponent/BaiTapLayoutTailwindCss";
-import BorderRadius from "./TailwindComponent/BorderRadius";
-import CustomCss from "./TailwindComponent/CustomCss";
-import DemoGrid from "./TailwindComponent/DemoGrid";
-import FlexDemo from "./TailwindComponent/FlexDemo";
-import PaddingMarginDemo from "./TailwindComponent/PaddingMarginDemo";
-import ResponsiveDemo from "./TailwindComponent/ResponsiveDemo";
-import TextBackGroundDemo from "./TailwindComponent/TextBackGroundDemo";
-import WidthHeightDemo from "./TailwindComponent/WidthHeightDemo";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <DemoGrid></DemoGrid>
-      <PaddingMarginDemo></PaddingMarginDemo>
-      <WidthHeightDemo></WidthHeightDemo> */}
-      {/* <FlexDemo></FlexDemo> */}
-      {/* <TextBackGroundDemo></TextBackGroundDemo>
-      <BorderRadius></BorderRadius> */}
-      {/* <ResponsiveDemo></ResponsiveDemo> */}
-      {/* <CustomCss></CustomCss> */}
-      {/* <BaiTapLayoutTailwindCss></BaiTapLayoutTailwindCss> */}
-      {/* <JoinDemo></JoinDemo> */}
-      {/* <LastFirstLodash></LastFirstLodash> */}
-      {/* <ChunkLodash></ChunkLodash> */}
-      {/* <FillLodash></FillLodash> */}
-      {/* <SortLodash></SortLodash> */}
-      {/* <IncludeLodash></IncludeLodash> */}
-      {/* <UniqLodash></UniqLodash> */}
-      {/* <FlattenLodash></FlattenLodash> */}
-      {/* <CompareObjectArray></CompareObjectArray> */}
-      <BaiTapTongHop></BaiTapTongHop>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
